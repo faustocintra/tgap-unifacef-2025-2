a64503013bf3bf3ab2c7f06bcb3fa625add168b7
Vitoria Hillary <125613539+vitoriahillary@users.noreply.github.com>
2025-08-26 22:31:53 +0000

    (26/08) exercicio cidades.js

--- cidades.js
+++ cidades.js
@@ -28,3 +28,25 @@ cidades.addEdge('Franca', 'Restinga')
 cidades.addEdge('Restinga', 'Batatais')
 cidades.addEdge('Franca', 'Batatais')
 export default cidades
+
+/* 
+
+2 - Não é possível representar diretamente duas ligações usando apenas listas de adjacência tradicionais.
+
+3 - Para possibilitar essa alteração, é preciso substituir a estrutura utilizada para armazenar as conexões. Atualmente, cada vértice guarda suas arestas em um Set, o que impede a duplicação. Para suportar mais de uma ligação, esse Set deve ser trocado por um Array.
+
+Atual: this.adjList.set(v, new Set())
+
+Modificada: this.adjList.set(v, [])
+
+Além disso, o método addEdge também deve ser ajustado. Hoje, ele utiliza o método .add, próprio do Set. Após a mudança para Array, será necessário usar o .push() para registrar as arestas, permitindo que o mesmo vértice apareça mais de uma vez
+
+Atual: this.adjList.get(v).add(w)
+if (!this.isDirected) this.adjList.get(w).add(v)
+
+Modificada: this.adjList.get(v).push(w)
+if (!this.isDirected) this.adjList.get(w).push(v)
+
+Dessa forma, a classe passa a representar um multigrafo, aceitando múltiplas arestas entre dois vértices distintos.
+
+*/
