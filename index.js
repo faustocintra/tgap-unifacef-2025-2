b8ceb56a18d03ce0eaffa66ec8fdd917f6e4f7a2
Alyson Monteiro <123329025+alyson-monteiro@users.noreply.github.com>
2025-08-26 20:14:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 import Graph from './lib/graph.class.js'
+import cidades from './cidades.js'
 
-const g = new Graph(false)  // false = não direcionado
 
-console.log(g)
-
-g.addVertex('A')
-g.addVertex('B')
-
-console.log('-'.repeat(80))
-
-console.log('Grafo após a adição de dois vértices:')
-console.log(g)
-
-console.log('-'.repeat(80))
-
-g.addEdge('A', 'B')
-
-console.log('Grafo após a adição da aresta A -> B:')
-console.log(g)
-
-console.log('-'.repeat(80))
-
-// Criando uma aresta diretamente. O vértice C ainda não
-// existe, e será criado automaticamente antes da criação
-// da aresta
-g.addEdge('A', 'C')
-
-console.log('Grafo após a adição da aresta A -> C:')
-console.log(g)
+console.log('Grafo de cidades:')
+console.log(cidades)
