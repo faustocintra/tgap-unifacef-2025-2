bc169be2d886d3ea2bd1cf6de87865d996ba1e92
Caio Taveira <124218355+caiotaveiraa@users.noreply.github.com>
2025-08-29 01:01:41 +0000

    Atividade reimplementação da classe grafos

---
+++ index-multi-edge.js
@@ -0,0 +1,60 @@
+import MultiEdgeGraph  from './lib/multi-edge-graph.class.js'
+
+const g = new MultiEdgeGraph (false) // false = não direcionado
+
+// Exercício 1 - Crie uma arquivo chamado cidades.js e, nele, represente em código o grafo da figura a seguir (não se preocupe com a distância entre as cidades).
+
+g.addVertex('Igarapava')
+g.addVertex('Ituverava')
+g.addVertex('Guará')
+g.addVertex('São Joaquim da Barra')
+g.addVertex('Jeriquara')
+g.addVertex('Ribeirão Corrente')
+g.addVertex('São José da Bela Vista')
+g.addVertex('Rifaina')
+g.addVertex('Pedregulho')
+g.addVertex('Cristais Paulista')
+g.addVertex('Franca')
+g.addVertex('Restinga')
+g.addVertex('Batatais')
+g.addVertex('Claraval')
+g.addVertex('Ibiraci')
+g.addVertex('Patrocínio Paulista')
+g.addVertex('Itirapuã')
+
+console.log('-'.repeat(80))
+
+console.log('Grafo após a adição dos vértices:')
+console.log(g)
+
+console.log('-'.repeat(80))
+
+g.addEdge('Igarapava', 'Ituverava')
+g.addEdge('Igarapava', 'Rifaina')
+g.addEdge('Igarapava', 'Jeriquara')
+g.addEdge('Ituverava', 'Jeriquara')
+g.addEdge('Ituverava', 'Guará')
+g.addEdge('Guará', 'Ribeirão Corrente')
+g.addEdge('Guará', 'São Joaquim da Barra')
+g.addEdge('São Joaquim da Barra', 'São José da Bela Vista')
+g.addEdge('Jeriquara', 'Pedregulho')
+g.addEdge('Jeriquara', 'Cristais Paulista')
+g.addEdge('Jeriquara', 'Ribeirão Corrente')
+g.addEdge('Ribeirão Corrente', 'Franca')
+g.addEdge('Ribeirão Corrente', 'Franca')
+g.addEdge('São José da Bela Vista', 'Franca')
+g.addEdge('Rifaina', 'Pedregulho')
+g.addEdge('Pedregulho', 'Cristais Paulista')
+g.addEdge('Cristais Paulista', 'Franca')
+g.addEdge('Franca', 'Claraval')
+g.addEdge('Franca', 'Ibiraci')
+g.addEdge('Franca', 'Patrocínio Paulista')
+g.addEdge('Franca', 'Batatais')
+g.addEdge('Franca', 'Restinga')
+g.addEdge('Restinga', 'Batatais')
+g.addEdge('Batatais', 'Patrocínio Paulista')
+g.addEdge('Claraval', 'Ibiraci')
+g.addEdge('Patrocínio Paulista', 'Itirapuã')
+
+console.log('Grafo após a criação das arestas')
+console.log(g)
